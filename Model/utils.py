21ec2fbf239f0459ef3ee7ba527e3fbb0b686544
Kyrie <huangyihua16@mails.ucas.ac.cn>
2019-05-30 18:44:10 +0800

    Update

--- Model/utils.py
+++ Model/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.sparse as sp
+import h5py
 from scipy.sparse.linalg.eigen.arpack import eigsh
 
 
@@ -53,3 +54,71 @@ def chebyshev_polynomials(adj, k):
         t_k.append(chebyshev_recurrence(t_k[-1], t_k[-2], scaled_laplacian))
 
     return sparse_to_tuple(t_k)
+
+
+def load_neighbour(neighbour, edges, is_padding=False):
+    data = neighbour
+
+    if is_padding == True:
+        x = np.zeros((edges + 1, 4)).astype('int32')
+
+        for i in range(0, edges):
+            x[i + 1] = data[:, i] + 1
+
+            for j in range(0, 4):
+                if x[i + 1][j] == -1:
+                    x[i + 1][j] = 0
+
+    else:
+        x = np.zeros((edges, 4)).astype('int32')
+
+        for i in range(0, edges):
+            x[i] = data[:, i]
+
+    return x
+
+
+def load_data(path, result_max=0.9, result_min=-0.9, logdr_ismap=False, s_ismap=False):
+    data = h5py.File(path)
+    logdr = data['logdr']
+    s = data['s']
+    e_neighbour = data['e_neighbour']
+    p_neighbour = np.transpose(data['p_neighbour'])
+    edgenum = len(logdr[0])
+    pointnum = len(s[0])
+    modelnum = len(logdr[0][0])
+    e_nb = load_neighbour(e_neighbour, edgenum)
+
+    maxdegree = p_neighbour.shape[1]
+    p_nb = p_neighbour
+    degree = np.zeros((p_neighbour.shape[0], 1)).astype('float32')
+    for i in range(p_neighbour.shape[0]):
+        degree[i] = np.count_nonzero(p_nb[i])
+
+    logdr_x = logdr
+    logdr_x = np.transpose(logdr_x, (2, 1, 0))
+
+    s_x = s
+    s_x = np.transpose(s_x, (2, 1, 0))
+    if logdr_ismap:
+        logdrmin = logdr_x.min() - 1e-6
+        logdrmax = logdr_x.max() + 1e-6
+
+        logdrnew = (result_max - result_min) * (logdr_x - logdrmin) / (logdrmax - logdrmin) + result_min
+
+    else:
+        logdrnew = logdr_x
+
+        logdrmin = 0
+        logdrmax = 0
+
+    if s_ismap:
+        smin = s_x.min() - 1e-6
+        smax = s_x.max() + 1e-6
+        snew = (result_max - result_min) * (s_x - smin) / (smax - smin) + result_min
+    else:
+        snew = s_x
+        smin = 0
+        smax = 0
+    return logdrnew, snew, e_nb, p_nb, degree, logdrmin, logdrmax, smin, smax, modelnum, pointnum, edgenum, maxdegree
+
